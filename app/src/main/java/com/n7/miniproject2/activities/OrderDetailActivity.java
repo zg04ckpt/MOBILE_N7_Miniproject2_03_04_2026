c3dd2ed0d365896bcd39edaf60d1ce64558fd5f0
hoangkhoi2k4 <lapyen30@gmail.com>
2026-04-03 14:30:01 +0700

    init activity

---
+++ app/src/main/java/com/n7/miniproject2/activities/OrderDetailActivity.java
@@ -0,0 +1,26 @@
+package com.n7.miniproject2.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.n7.miniproject2.R;
+
+public class OrderDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_order_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
